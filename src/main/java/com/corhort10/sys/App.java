ad8934f60c488030f728e7bfd3670ab97f0b654b
Jay-korir <sandeep.simplelearn.net>
2022-09-25 21:05:35 +0300

    test project

---
+++ src/main/java/com/corhort10/sys/App.java
@@ -0,0 +1,13 @@
+package com.corhort10.sys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
